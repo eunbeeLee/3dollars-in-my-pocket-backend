e8e0792baf052c4bd94366036816eba07c268478
Seungho Kang <will.seungho@gmail.com>
2021-07-01 01:07:44 +0900

    [TD-18] LocationDistanceUtils 변수명 수정.

--- threedollar-common/src/main/java/com/depromeet/threedollar/common/utils/LocationDistanceUtils.java
+++ threedollar-common/src/main/java/com/depromeet/threedollar/common/utils/LocationDistanceUtils.java
@@ -6,9 +6,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LocationDistanceUtils {
 
-    public static Integer getDistance(double lat1, double lon1, double lat2, double lon2) {
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+    public static Integer getDistance(double sourceLatitude, double sourceLongitude, double targetLatitude, double targetLongitude) {
+        double theta = sourceLongitude - targetLongitude;
+        double dist = Math.sin(deg2rad(sourceLatitude)) * Math.sin(deg2rad(targetLatitude))
+            + Math.cos(deg2rad(sourceLatitude)) * Math.cos(deg2rad(targetLatitude)) * Math.cos(deg2rad(theta));
         dist = Math.acos(dist);
         dist = rad2deg(dist);
         dist = dist * 60 * 1.1515;
