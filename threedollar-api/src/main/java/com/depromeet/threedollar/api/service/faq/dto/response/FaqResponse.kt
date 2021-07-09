5c0fef999aa6007b0f7d98844a13d9fd1e404cd2
Seungho Kang <will.seungho@gmail.com>
2021-07-10 07:00:28 +0900

    [TD-32] id -> faqId 수정 및 Auditing time 필드 추가.

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/service/faq/dto/response/FaqResponse.kt
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/service/faq/dto/response/FaqResponse.kt
@@ -1,18 +1,22 @@
 package com.depromeet.threedollar.api.service.faq.dto.response
 
+import com.depromeet.threedollar.api.dto.AudtingTimeResponse
+import com.depromeet.threedollar.domain.domain.common.AuditingTimeEntity
 import com.depromeet.threedollar.domain.domain.faq.Faq
 import com.depromeet.threedollar.domain.domain.faq.FaqCategory
 
 data class FaqResponse(
-    val id: Long?,
+    val faqId: Long?,
     val question: String,
     val answer: String,
     val category: FaqCategory
-) {
+) : AudtingTimeResponse() {
 
     companion object {
         fun of(faq: Faq): FaqResponse {
-            return FaqResponse(faq.id, faq.question, faq.answer, faq.category)
+            val response = FaqResponse(faq.id, faq.question, faq.answer, faq.category)
+            response.setBaseTime(faq)
+            return response
         }
     }
 
