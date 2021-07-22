f1af84c755923e814d0171bd6efe37a42577bba0
Seungho Kang <will.seungho@gmail.com>
2021-07-22 17:03:36 +0900

    [TD-65] 회원가입시 입력하는 닉네임 검증 추가.

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/service/auth/dto/request/SignUpRequest.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/service/auth/dto/request/SignUpRequest.java
@@ -1,5 +1,6 @@
 package com.depromeet.threedollar.api.service.auth.dto.request;
 
+import com.depromeet.threedollar.api.config.validator.NickName;
 import com.depromeet.threedollar.api.service.user.dto.request.CreateUserRequest;
 import com.depromeet.threedollar.domain.domain.user.UserSocialType;
 import lombok.*;
@@ -17,6 +18,7 @@ public class SignUpRequest {
     private String token;
 
     @NotBlank
+    @NickName
     private String name;
 
     @NotNull
