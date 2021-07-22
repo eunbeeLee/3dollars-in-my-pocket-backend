e16ff339459b95b637f746f0fcb6cf100b13514e
Seungho Kang <will.seungho@gmail.com>
2021-07-23 02:07:27 +0900

    [TD-78] 관리자 서버 - FAQ를 등록하는 API 수정 및 http client 추가.

--- threedollar-admin/src/main/kotlin/com/depromeet/threedollar/admin/controller/faq/FaqController.kt
+++ threedollar-admin/src/main/kotlin/com/depromeet/threedollar/admin/controller/faq/FaqController.kt
@@ -6,6 +6,7 @@ import com.depromeet.threedollar.admin.service.faq.dto.request.AddFaqRequest
 import com.depromeet.threedollar.admin.service.faq.dto.response.FaqResponse
 import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,7 +16,7 @@ class FaqController(
 
     @ApiOperation("새로운 FAQ를 등록합니다.")
     @PostMapping("/admin/v1/faq")
-    fun addFaq(request: AddFaqRequest): ApiResponse<FaqResponse> {
+    fun addFaq(@RequestBody request: AddFaqRequest): ApiResponse<FaqResponse> {
         return ApiResponse.success(faqService.addFaq(request))
     }
 
