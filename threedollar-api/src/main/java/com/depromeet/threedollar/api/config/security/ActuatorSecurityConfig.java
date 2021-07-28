9e0ed15ca7d8ac8b1c44402fdc418d4f3bd98dde
Seungho Kang <will.seungho@gmail.com>
2021-07-29 06:34:11 +0900

    [TD-97] 시큐리티 설정 추가.

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/config/security/ActuatorSecurityConfig.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/config/security/ActuatorSecurityConfig.java
@@ -2,10 +2,20 @@ package com.depromeet.threedollar.api.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+            .antMatchers(
+                "/h2-console/**",
+                "/swagger-ui.html/**"
+            );
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -15,7 +25,8 @@ public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .httpBasic()
             .and()
-                .csrf().disable();
+                .csrf().disable()
+            .headers().frameOptions();
     }
 
 }
