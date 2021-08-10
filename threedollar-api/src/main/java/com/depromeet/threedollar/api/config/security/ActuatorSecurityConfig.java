43abf3b3bc71b410b7daddd37dbb6b18de6e47ac
seungh0 <will.seungho@gmail.com>
2021-08-10 10:32:38 +0900

    [TD-148] 모니터링에서 서버 상태 확인을 위해 인증시 매번 세션을 생성하는 문제 해결

--- threedollar-api/src/main/java/com/depromeet/threedollar/api/config/security/ActuatorSecurityConfig.java
+++ threedollar-api/src/main/java/com/depromeet/threedollar/api/config/security/ActuatorSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,8 +26,10 @@ public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .httpBasic()
             .and()
-                .csrf().disable()
-            .headers().frameOptions();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .csrf().disable();
     }
 
 }
