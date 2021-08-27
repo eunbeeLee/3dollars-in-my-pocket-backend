2a8e08f647860120b118108f1b8af422590175a9
seungh0 <will.seungho@gmail.com>
2021-08-27 22:16:10 +0900

    [TD-173] 관리자가 FAQ 변경시, 해당 카테고리와 전체 카테고리 캐시 삭제

--- threedollar-admin/src/main/kotlin/com/depromeet/threedollar/admin/service/faq/FaqAdminService.kt
+++ threedollar-admin/src/main/kotlin/com/depromeet/threedollar/admin/service/faq/FaqAdminService.kt
@@ -2,8 +2,11 @@ package com.depromeet.threedollar.admin.service.faq
 
 import com.depromeet.threedollar.admin.service.faq.dto.request.AddFaqRequest
 import com.depromeet.threedollar.admin.service.faq.dto.request.UpdateFaqRequest
+import com.depromeet.threedollar.application.config.cache.CacheType
 import com.depromeet.threedollar.application.service.faq.dto.response.FaqResponse
 import com.depromeet.threedollar.domain.domain.faq.FaqRepository
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Caching
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,11 +15,23 @@ class FaqAdminService(
     private val faqRepository: FaqRepository
 ) {
 
+    @Caching(
+        evict = [
+            CacheEvict(key = "#request.category", value = [CacheType.CacheKey.FAQS]),
+            CacheEvict(key = "'ALL'", value = [CacheType.CacheKey.FAQS])
+        ]
+    )
     @Transactional
     fun addFaq(request: AddFaqRequest): FaqResponse {
         return FaqResponse.of(faqRepository.save(request.toEntity()))
     }
 
+    @Caching(
+        evict = [
+            CacheEvict(key = "#request.category", value = [CacheType.CacheKey.FAQS]),
+            CacheEvict(key = "'ALL'", value = [CacheType.CacheKey.FAQS])
+        ]
+    )
     @Transactional
     fun updateFaq(faqId: Long, request: UpdateFaqRequest): FaqResponse {
         val faq = FaqAdminServiceUtils.findFaqById(faqRepository, faqId)
@@ -24,6 +39,12 @@ class FaqAdminService(
         return FaqResponse.of(faq)
     }
 
+    @Caching(
+        evict = [
+            // TODO 파라미터로 카테고리가 넘어오지 않는 경우 대응
+            CacheEvict(key = "'ALL'", value = [CacheType.CacheKey.FAQS])
+        ]
+    )
     @Transactional
     fun deleteFaq(faqId: Long) {
         val faq = FaqAdminServiceUtils.findFaqById(faqRepository, faqId)
