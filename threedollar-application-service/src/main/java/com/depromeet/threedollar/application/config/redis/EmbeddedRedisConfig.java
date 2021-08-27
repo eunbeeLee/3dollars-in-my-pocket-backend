398a570b7eba644cbf547b37a4206fc416b160c9
seungh0 <will.seungho@gmail.com>
2021-08-27 11:57:35 +0900

    [TD-170] Redis 설정 프로파일 관리 변경

--- threedollar-application-service/src/main/java/com/depromeet/threedollar/application/config/redis/EmbeddedRedisConfig.java
+++ threedollar-application-service/src/main/java/com/depromeet/threedollar/application/config/redis/EmbeddedRedisConfig.java
@@ -2,11 +2,8 @@ package com.depromeet.threedollar.application.config.redis;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
@@ -29,24 +26,19 @@ public class EmbeddedRedisConfig {
 
     private RedisServer redisServer;
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(properties.getHost(), properties.getPort());
-    }
-
     @PostConstruct
     public void redisServer() throws IOException {
         int port = isRunningPort(properties.getPort()) ? findAvailableRandomPort() : properties.getPort();
-        log.info("인메모리 레디스 서버가 기동되었습니다. port {}", port);
         redisServer = new RedisServer(port);
         redisServer.start();
+        log.info("임베디드 레디스 서버가 기동되었습니다. port: {}", port);
     }
 
     @PreDestroy
     public void stopRedis() {
         if (redisServer != null) {
             redisServer.stop();
-            log.info("인메모리 레디스 서버가 종료됩니다");
+            log.info("임베디드 레디스 서버가 종료됩니다");
         }
     }
 
--- threedollar-application-service/src/main/java/com/depromeet/threedollar/application/config/redis/RedisConfig.java
+++ threedollar-application-service/src/main/java/com/depromeet/threedollar/application/config/redis/RedisConfig.java
@@ -3,12 +3,10 @@ package com.depromeet.threedollar.application.config.redis;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-@Profile({"dev", "prod"})
 @RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
