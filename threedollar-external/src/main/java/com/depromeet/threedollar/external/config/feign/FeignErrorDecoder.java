978988302123504cb329a7046eabfbf5384a7b18
seungh0 <will.seungho@gmail.com>
2021-10-07 02:56:55 +0900

    [TD-217] FeignErrorDecode 코드 리팩토링

--- threedollar-external/src/main/java/com/depromeet/threedollar/external/config/feign/FeignErrorDecoder.java
+++ threedollar-external/src/main/java/com/depromeet/threedollar/external/config/feign/FeignErrorDecoder.java
@@ -9,16 +9,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class FeignErrorDecoder implements ErrorDecoder {
 
+    private static final int MIN_CLIENT_ERROR_STATUS_CODE = 400;
+    private static final int MAX_CLIENT_ERROR_STATUS_CODE = 499;
+
     @Override
     public Exception decode(String methodKey, Response response) {
         if (isClientError(response.status())) {
-            return new ValidationException(String.format("외부 API 호출 중 에러가 발생하였습니다. (%s) (%s) (%s)", methodKey, response.status(), response.body()));
+            return new ValidationException(String.format("외부 API 호출 중 클라이언트 에러(%s)가 발생하였습니다. message: (%s)", response.status(), response.body()));
         }
-        return new BadGatewayException(String.format("외부 API 연동 중 에러가 발생하였습니다. (%s) (%s)", response.status(), response.body()));
+        return new BadGatewayException(String.format("외부 API 연동 중 서버 에러(%s)가 발생하였습니다. message: (%s)", response.status(), response.body()));
     }
 
     private boolean isClientError(int status) {
-        return 400 <= status && status < 500;
+        return MIN_CLIENT_ERROR_STATUS_CODE <= status && status <= MAX_CLIENT_ERROR_STATUS_CODE;
     }
 
 }
